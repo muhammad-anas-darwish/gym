1bd9ef80b06db95409b9e3fce0f3cfd83cc25360
Muhammad-Anas-Darwish <muhammadanasdarwish1@gmail.com>
2024-03-06 12:42:55 +0300

    add validate route parameters

--- routes/api.php
+++ routes/api.php
@@ -46,11 +46,11 @@ Route::apiResource('advices', AdviceController::class)->only(['index', 'store', 
 Route::apiResource('articles', ArticleController::class)->only(['index', 'store', 'show', 'update', 'destroy']);
 Route::apiResource('meals', MealController::class)->only(['store', 'update', 'destroy']);
 Route::get('/meals/my', [MealController::class, 'getUserMeals'])->name('meals.getUserMeals');
-Route::get('/meals/{userId}', [MealController::class, 'getMealsOfUser'])->name('meals.getMealOfUser')->where('user_id', '[0-9]+');
+Route::get('/meals/{userId}', [MealController::class, 'getMealsOfUser'])->name('meals.getMealOfUser')->whereNumber('userId');
 Route::apiResource('meal_food', MealFoodController::class)->only(['store', 'update', 'destroy']);
-Route::get('/meal_food/{mealId}', [MealFoodController::class, 'getFoods'])->name('meals.getFoods')->where('meal_id', '[0-9]+');
+Route::get('/meal_food/{mealId}', [MealFoodController::class, 'getFoods'])->name('meals.getFoods')->whereNumber('mealId');
 Route::apiResource('videos', VideoController::class)->only(['index', 'store', 'show', 'update', 'destroy']);
 Route::apiResource('user_exercise', UserExerciseController::class)->only(['store', 'update', 'destroy']);
 Route::get('/user_exercise/my', [UserExerciseController::class, 'getExercises'])->name('user_exercise.getExercises');
-Route::get('/user_exercise/{userId}', [UserExerciseController::class, 'getUserExercises'])->name('user_exercise.getUserExercises')->where('user_id', '[0-9]+');
+Route::get('/user_exercise/{userId}', [UserExerciseController::class, 'getUserExercises'])->name('user_exercise.getUserExercises')->whereNumber('userId');
 Route::apiResource('user_chat', UserChatController::class)->only(['store', 'destroy']);
